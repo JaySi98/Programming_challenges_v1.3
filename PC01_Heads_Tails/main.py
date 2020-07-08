5eaff529f6ae550b6178b51178a8d623917f14c1
Jakub Szkiłądź <jakub.szkiladz@wp.pl>
2020-07-08 18:14:13 +0200

    created heads tails program

--- PC01_Heads_Tails/main.py
+++ PC01_Heads_Tails/main.py
@@ -1,5 +1,35 @@
+import getopt
+import sys
 import random
-import numpy as np
-from matplotlib import pyplot as pl
 
 
+def get_arg(argv):
+    num = 1
+    
+    try:
+      opts, args = getopt.getopt(argv,'n:')
+    except getopt.GetoptError:
+      print('invalid arguments')
+      sys.exit()
+    else:
+        for opt, arg in opts:
+            if opt == '-n':
+                num = arg
+                return num
+        return num
+
+
+if __name__ == '__main__':
+    num = int(get_arg(sys.argv[1:]))
+    heads, tails = 0, 0
+
+    for i in range(num):
+        r = random.randint(0,1)
+        if r:
+            heads += 1
+        else:
+            tails += 1
+
+    print('Number of coin flips: ', num)
+    print('Number of heads: ', heads)
+    print('Number of tails: ', tails)
