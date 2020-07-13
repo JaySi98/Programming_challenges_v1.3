fbcfc6ce893c9b9595c70ab7c56c8b8963c268db
Jakub Szkiłądź <jakub.szkiladz@wp.pl>
2020-07-13 19:39:40 +0200

    created 'Rock Paper Scissors' game

---
+++ PC06_Rock_Paper_Scissors/GameLogic.py
@@ -0,0 +1,18 @@
+import sys
+import pygame
+
+#CHECKING EVENTS ----------------------------------------------------------- #
+#checks game events such ass closing the game
+def check_events(event):
+    if event.type == pygame.QUIT:
+        pygame.quit()
+        sys.exit()
+
+
+#DRAWING -----------------------------------------------------------
+#refreshes the window
+def update_window(window, background):
+    window.blit(background.texture, background.rect)
+
+
+    pygame.display.flip()
---
+++ PC06_Rock_Paper_Scissors/Settings.py
@@ -0,0 +1,10 @@
+class Settings():
+
+    def __init__(self):
+            
+        #screen settings
+        self.screen_dim = (840, 600)
+
+        # image paths
+        self.background_path = 'textures/back.bmp'
+        
