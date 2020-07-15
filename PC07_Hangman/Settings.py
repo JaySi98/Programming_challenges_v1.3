3c39035b5c772d7827bd3b576ab7fa9b84259de9
Jakub Szkiłądź <jakub.szkiladz@wp.pl>
2020-07-15 15:23:24 +0200

    created 'Hangman' game

---
+++ PC07_Hangman/Settings.py
@@ -0,0 +1,8 @@
+# SETTINGS ----------------------------------------------------------------- #
+class Settings():
+    def __init__(self):
+        #screen settings
+        self.screen_dim = (840, 600)
+
+        # image paths
+        self.background_path = 'textures/back.bmp'
---
+++ PC07_Hangman/main.py
@@ -0,0 +1,33 @@
+import random
+import pygame
+#
+import GameLogic as gl
+from Background import Background
+from Settings import Settings
+
+
+# MAIN --------------------------------------------------------------------- #
+if __name__ == '__main__':
+    #settings initialization
+    settings = Settings()
+
+    #screen initialization
+    pygame.init()
+    window = pygame.display.set_mode(settings.screen_dim)
+    pygame.display.set_caption("Hangman")
+
+    #loading background texture
+    background = Background(settings)
+
+    #loading gesture textures
+
+    #loading button textures 
+
+    # MAIN LOOP ------------------------------------------------------------ #
+    while True:
+        #CHECKING EVENTS --------------------------------------------------- #
+        for event in pygame.event.get():
+            gl.check_events(event)
+
+        #updating window with new matrix
+        gl.update_window(window, background)
