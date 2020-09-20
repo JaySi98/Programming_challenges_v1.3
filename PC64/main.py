4768c332ab242eb96aa2ea9b249a48a17ca62bd3
Jakub Szkiłądź <jakub.szkiladz@wp.pl>
2020-09-20 19:35:18 +0200

    started Ulam Spiral

---
+++ PC64/main.py
@@ -0,0 +1,25 @@
+import sys
+import PIL
+
+# PARSE ARGUMENT ------------------------------------------------------------- #
+def parse_argument(argv):
+    return 100
+
+
+# MAIN --------------------------------------------------------------------- #
+if __name__ == '__main__':
+    # INITIALIZATION ------------------------------------------------------- #
+    # parsing arguments to get the lenght of the spiral
+    lenght = parse_argument(sys.argv)
+
+    # creating array 
+
+    # creating image 
+
+    '''        
+    im = Image.new("RGB", (newwidth, height))
+    im.putdata(colors)
+    im.save("Picture", "PNG")
+    '''
+
+
