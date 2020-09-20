2c26306ecdbd489f4286eccc0efa7fc95e38de89
Jakub Szkiłądź <jakub.szkiladz@wp.pl>
2020-09-20 19:55:31 +0200

    added arguments parsing

--- PC64/main.py
+++ PC64/main.py
@@ -1,17 +1,27 @@
+import argparse
 import sys
 import PIL
 
 # PARSE ARGUMENT ------------------------------------------------------------- #
 def parse_argument(argv):
-    return 100
+    if len(argv) < 2:
+        print('Error, not enough arguments')
+        sys.exit()
+    parser = argparse.ArgumentParser(description='Ulam Spiral')
+    parser.add_argument('-n','--num', type=int, help='lenght of the spiral')
+    args = parser.parse_args()
+    if 1 <= args.num <= 600:
+        return args.num
+    else: 
+        print('Error, incorrect argument')
 
 
 # MAIN --------------------------------------------------------------------- #
 if __name__ == '__main__':
-    # INITIALIZATION ------------------------------------------------------- #
     # parsing arguments to get the lenght of the spiral
     lenght = parse_argument(sys.argv)
-
+    print('lenght:', lenght)
+    
     # creating array 
 
     # creating image 
